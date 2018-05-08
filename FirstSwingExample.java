41c0c8001004b567b011afe73b19aa4767f821fb
sfmichels <sf.michels@gmail.com>
2018-05-08 16:28:55 -0700

    Use readable names, better spacing and better indentation.

--- FirstSwingExample.java
+++ FirstSwingExample.java
@@ -7,16 +7,16 @@
 
 import javax.swing.*;  
 public class FirstSwingExample {  
-public static void main(String[] args) {  
-JFrame f=new JFrame();//creating instance of JFrame  
+  public static void main(String[] args) {  
+    JFrame frame = new JFrame();  //creating instance of JFrame  
           
-JButton b=new JButton("click");//creating instance of JButton  
-b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+    JButton button = new JButton("click");  //creating instance of JButton  
+    button.setBounds(130,100,100, 40);      //x axis, y axis, width, height  
           
-f.add(b);//adding button in JFrame  
+    frame.add(button);  //adding button in JFrame  
           
-f.setSize(400,500);//400 width and 500 height  
-f.setLayout(null);//using no layout managers  
-f.setVisible(true);//making the frame visible  
-}  
+    frame.setSize(400,500);  //400 width and 500 height  
+    frame.setLayout(null);   //using no layout managers  
+    frame.setVisible(true);  //making the frame visible  
+  }  
 }  
